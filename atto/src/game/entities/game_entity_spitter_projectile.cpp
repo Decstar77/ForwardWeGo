b1cf50869791897755a34d97f78cbd0e620e06b8
Decstar77 <34284628+Decstar77@users.noreply.github.com>
2026-03-30 03:30:55 +0200

    Q

---
+++ atto/src/game/entities/game_entity_spitter_projectile.cpp
@@ -0,0 +1,5 @@
+//
+// Created by porte on 3/29/2026.
+//
+
+#include "game_entity_spitter_projectile.h"
---
+++ atto/src/game/entities/game_entity_spitter_projectile.h
@@ -0,0 +1,16 @@
+//
+// Created by porte on 3/29/2026.
+//
+
+#ifndef GAME_ENTITY_SPITTER_PROJECTILE_H
+#define GAME_ENTITY_SPITTER_PROJECTILE_H
+
+
+
+class GameEntitySpitterProjectile {
+
+};
+
+
+
+#endif //GAME_ENTITY_SPITTER_PROJECTILE_H
